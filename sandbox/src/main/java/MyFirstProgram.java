b59443af55726590305579c7ee114c47d701184f
ryzhkova_nat <natalya_ryzhkova@relex.ru>
2019-09-18 15:43:24 +0300

    A function of calculating a distanse between 2 points has been added

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -1,7 +1,21 @@
+import static java.lang.Math.sqrt;
+
 public class MyFirstProgram {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, world!");
+		//System.out.println("Hello, world!");
+
+		Point p1 = new Point();
+		p1.a = 1;
+		p1.b = 2;
+		Point p2 = new Point();
+		p2.a = 3;
+		p2.b = 4;
+		System.out.println("Расстояние между точками p1(" + p1.a + ";" + p1.b + ") и p2(" + p2.a + ";" + p2.b + ") равно " + distance(p1, p2));
 	}
 
+	public static double distance (Point p1, Point p2) {
+
+		return sqrt((p1.a-p1.b)*(p2.a-p2.b));
+	}
 }
