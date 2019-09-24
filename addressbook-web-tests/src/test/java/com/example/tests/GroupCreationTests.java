05ec5fb928fc72f8e28f092e9e633aa55258b72c
ryzhkova_nat <natalya_ryzhkova@relex.ru>
2019-09-24 15:54:34 +0300

    Group Data class has been created

--- addressbook-web-tests/src/test/java/com/example/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/com/example/tests/GroupCreationTests.java
@@ -38,7 +38,7 @@ public class GroupCreationTests {
     public void testAddGroup() throws Exception {
         goToGroupPage();
         initGroupCreation();
-        fillGroupForm();
+        fillGroupForm(new GroupData("test1", "testheader", "testfooter"));
         submitGroupCreation();
         ReturnToGroupPage();
     }
@@ -51,16 +51,16 @@ public class GroupCreationTests {
         driver.findElement(By.name("submit")).click();
     }
 
-    private void fillGroupForm() {
+    private void fillGroupForm(GroupData groupData) {
         driver.findElement(By.name("group_name")).click();
         driver.findElement(By.name("group_name")).clear();
-        driver.findElement(By.name("group_name")).sendKeys("test1");
+        driver.findElement(By.name("group_name")).sendKeys(groupData.getName());
         driver.findElement(By.name("group_header")).click();
         driver.findElement(By.name("group_header")).clear();
-        driver.findElement(By.name("group_header")).sendKeys("testheader");
+        driver.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
         driver.findElement(By.name("group_footer")).click();
         driver.findElement(By.name("group_footer")).clear();
-        driver.findElement(By.name("group_footer")).sendKeys("testfooter");
+        driver.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
     }
 
     private void initGroupCreation() {
