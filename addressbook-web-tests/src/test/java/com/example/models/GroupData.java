4e32483c8dfa997383754e3e9e5e524a24fa8191
ryzhkova_nat <natalya_ryzhkova@relex.ru>
2019-10-14 17:20:21 +0300

    Checks for groups have been added

--- addressbook-web-tests/src/test/java/com/example/models/GroupData.java
+++ addressbook-web-tests/src/test/java/com/example/models/GroupData.java
@@ -1,14 +1,54 @@
 package com.example.models;
 
+import java.util.Objects;
+
 public class GroupData {
     private final String name;
     private final String header;
     private final String footer;
+    private int id;
 
-    public GroupData(String name, String header, String footer) {
+    public GroupData(int id, String name, String header, String footer) {
+        this.id = id;
         this.name = name;
         this.header = header;
         this.footer = footer;
+    }
+
+    @Override
+    public String toString() {
+        return "GroupData{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GroupData groupData = (GroupData) o;
+        return Objects.equals(name, groupData.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    public GroupData(String name, String header, String footer) {
+        this.id = Integer.MAX_VALUE;
+        this.name = name;
+        this.header = header;
+        this.footer = footer;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public String getName() {
--- addressbook-web-tests/src/test/java/com/example/tests/EditGroupTests.java
+++ addressbook-web-tests/src/test/java/com/example/tests/EditGroupTests.java
@@ -4,6 +4,9 @@ import com.example.models.GroupData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
 
 public class EditGroupTests extends TestBase {
@@ -17,10 +20,18 @@ public class EditGroupTests extends TestBase {
         List<GroupData> before = app.getGroupHelper().getGroupList();
         app.getGroupHelper().selectGroup(before.size() - 1);
         app.getGroupHelper().editGroup();
-        app.getGroupHelper().fillGroupForm(new GroupData("sample", "group", "name"));
+        GroupData group = new GroupData(before.get(before.size()-1).getId(), "sample", "group", "name");
+        app.getGroupHelper().fillGroupForm(group);
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnToGroupPage();
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size());
+
+        before.remove(before.size()-1);
+        before.add(group);
+        Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
+        before.sort(byId);
+        after.sort(byId);
+        Assert.assertEquals(before, after);
     }
 }
