2131d61c2dec345ef1e7d3cf98cd91812f7b511b
ryzhkova_nat <natalya_ryzhkova@relex.ru>
2019-11-24 21:46:33 +0300

    Ex. 12 completed

---
+++ addressbook-web-tests/src/test/java/com/example/generators/ContactDataGenerator.java
@@ -0,0 +1,77 @@
+package com.example.generators;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
+import com.example.models.ContactData;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ContactDataGenerator {
+
+    @Parameter(names = "-c", description = "contact count")
+    public int count;
+
+    @Parameter(names = "-f", description = "file")
+    public String file;
+
+    @Parameter(names = "-d", description = "format")
+    public String format;
+
+    public static void main(String[] args) throws IOException {
+        ContactDataGenerator generator = new ContactDataGenerator();
+        JCommander jCommander = new JCommander(generator);
+        try {
+            jCommander.parse(args);
+        } catch (ParameterException ex) {
+            jCommander.usage();
+            return;
+        }
+        generator.run();
+    }
+
+    private void run() throws IOException {
+        List<ContactData> contacts = generateContacts(count);
+        if (format.equals("csv")) {
+            saveAsCSV(contacts, new File(file));
+        } else if (format.equals("json")) {
+            saveAsJson(contacts, new File(file));
+        } else {
+            System.out.println("Unrecognised format" + format);
+        }
+    }
+
+    private List<ContactData> generateContacts(int count) {
+        List<ContactData> contacts = new ArrayList<ContactData>();
+        for (int i = 0; i < count; i++) {
+            contacts.add(new ContactData().withFirstName(String.format("test name %s", i))
+                    .withLastName(String.format("test surname %s", i)).withAddress(String.format("address %s", i)).withEmail(String.format("email %s", i)).withMobilePhone(String.format("12345%s", i)).withHomePhone(String.format("67890%s", i)).withWorkPhone(String.format("13579%s", i)));
+        }
+        return contacts;
+    }
+
+    private void saveAsCSV(List<ContactData> contacts, File file) throws IOException {
+        System.out.println(new File(".").getAbsolutePath());
+        Writer writer = new FileWriter(file);
+        for (ContactData contact : contacts) {
+            writer.write(String.format("%s, %s, %s, %s, %s, %s, %s\n", contact.getFirstName(), contact.getLastName(), contact.getAddress(), contact.getEmail(), contact.getMobilePhone(), contact.getHomePhone(), contact.getWorkPhone()));
+        }
+        writer.close();
+    }
+
+    private void saveAsJson(List<ContactData> groups, File file) throws IOException {
+
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String json = gson.toJson(groups);
+        Writer writer = new FileWriter(file);
+        writer.write(json);
+        writer.close();
+    }
+}
